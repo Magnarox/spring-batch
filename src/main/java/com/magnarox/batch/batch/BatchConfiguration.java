9a409a4e84c65f284d1e3429418114fe3b5e0cdc
Magnarox <magnarox06@gmail.com>
2020-03-02 23:03:10 +0100

    Partitioned Job with custom Partitioner

--- src/main/java/com/magnarox/batch/batch/BatchConfiguration.java
+++ src/main/java/com/magnarox/batch/batch/BatchConfiguration.java
@@ -7,16 +7,19 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.MultiResourceItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,23 +37,11 @@ public class BatchConfiguration {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public MultiResourceItemReader<TutoPeople> multiResourceItemReader() throws IOException {
-        final File inputDir = new ClassPathResource("input").getFile();
-        if (!inputDir.exists() || !inputDir.isDirectory())
-            throw new IOException("Bad input configuration");
-
-        final ClassPathResource[] inputResources = Arrays.stream(inputDir.list()).map(x -> "input/" + x).map(ClassPathResource::new).toArray(ClassPathResource[]::new);
-
-        final MultiResourceItemReader<TutoPeople> resourceItemReader = new MultiResourceItemReader<TutoPeople>();
-        resourceItemReader.setResources(inputResources);
-        resourceItemReader.setDelegate(reader());
-        return resourceItemReader;
-    }
-
-    @Bean
-    public FlatFileItemReader<TutoPeople> reader() {
+    @StepScope
+    public FlatFileItemReader<TutoPeople> reader(@Value("#{stepExecutionContext[fileName]}") String filename) {
         return new FlatFileItemReaderBuilder<TutoPeople>()
                 .name("personReader")
+                .resource(new ClassPathResource("input/" + filename))
                 .delimited()
                 .names(new String[]{"firstName", "lastName"})
                 .fieldSetMapper(new BeanWrapperFieldSetMapper<TutoPeople>() {{
@@ -60,33 +51,63 @@ public class BatchConfiguration {
     }
 
     @Bean
+    public ResourcePartitioner partitioner() throws IOException {
+        return new ResourcePartitioner().setResources(getResources());
+    }
+
+    @Bean
     public PersonItemProcessor processor() {
         return new PersonItemProcessor();
     }
 
     @Bean
-    public JpaRepositoryItemWriter<TutoPeople> writer(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") final TutoPeopleRepository repository) {
-
+    @StepScope
+    public JpaRepositoryItemWriter<TutoPeople> writer(final TutoPeopleRepository repository) {
         return new JpaRepositoryItemWriter<>(repository);
     }
 
     @Bean
-    public Job importUserJob(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") final BatchJobExecutionListener listener, Step step1) {
-        return jobBuilderFactory.get("importUserJob")
-                .incrementer(new RunIdIncrementer())
+    public Job partitionerJob(final BatchJobExecutionListener listener, final Step partitionedStep) {
+        return jobBuilderFactory.get("partitionerJob")
                 .listener(listener)
-                .flow(step1)
-                .end()
+                .start(partitionedStep)
                 .build();
     }
 
     @Bean
-    public Step step1(final JpaRepositoryItemWriter<TutoPeople> writer) throws IOException {
-        return stepBuilderFactory.get("step1")
-                .<TutoPeople, TutoPeople>chunk(10)
-                .reader(multiResourceItemReader())
+    public Step partitionedStep(final JpaRepositoryItemWriter<TutoPeople> writer) throws IOException {
+        return stepBuilderFactory.get("partitionStep")
+                .partitioner("slaveStep", partitioner())
+                .step(slaveStep(writer))
+                .taskExecutor(taskExecutor())
+                .build();
+    }
+
+    @Bean
+    public Step slaveStep(final JpaRepositoryItemWriter<TutoPeople> writer) {
+        return stepBuilderFactory.get("slaveStep")
+                .<TutoPeople, TutoPeople>chunk(1)
+                .reader(reader(null))
                 .processor(processor())
                 .writer(writer)
                 .build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setMaxPoolSize(5);
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setQueueCapacity(5);
+        taskExecutor.afterPropertiesSet();
+        return taskExecutor;
+    }
+
+    private Resource[] getResources() throws IOException {
+        final File inputDir = new ClassPathResource("input").getFile();
+        if (!inputDir.exists() || !inputDir.isDirectory())
+            throw new IOException("Bad input configuration");
+
+        return Arrays.stream(inputDir.list()).map(x -> "input/" + x).map(ClassPathResource::new).toArray(ClassPathResource[]::new);
     }
 }
