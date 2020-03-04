05e6be4d917b69dbc5a5909211bd4853c4989114
Magnarox <magnarox06@gmail.com>
2020-03-05 00:37:57 +0100

    Run asynchronous task

--- manager/src/main/java/com/magnarox/batch/manager/EntryPoint.java
+++ manager/src/main/java/com/magnarox/batch/manager/EntryPoint.java
@@ -1,10 +1,13 @@
 package com.magnarox.batch.manager;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,23 +25,29 @@ public class EntryPoint {
     @Autowired
     private Job importUserJob;
 
-    @Autowired
-    private JobLauncher jobLauncher;
+    private final SimpleJobLauncher jobLauncher;
+
+    public EntryPoint(JobRepository jobRepository) {
+        this.jobLauncher = new SimpleJobLauncher();
+        this.jobLauncher.setJobRepository(jobRepository);
+        this.jobLauncher.setTaskExecutor(new SimpleAsyncTaskExecutor());
+    }
 
     @GetMapping("start")
     public ResponseEntity<String> start() {
         try {
-            Map<String, JobParameter> parameters = new HashMap<>();
-            JobParameter param = new JobParameter(UUID.randomUUID().toString(), true);
+            final Map<String, JobParameter> parameters = new HashMap<>();
+            final JobParameter param = new JobParameter(UUID.randomUUID().toString(), true);
             parameters.put("UUID", param);
-            JobParameters jobParameters = new JobParameters(parameters);
+            final JobParameters jobParameters = new JobParameters(parameters);
 
-            jobLauncher.run(importUserJob, jobParameters);
+            final JobExecution jobExecution = this.jobLauncher.run(importUserJob, jobParameters);
+            final Long jobId = jobExecution.getJobId();
+
+            return ResponseEntity.ok("Job " + jobId + " is started.");
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
-
-        return ResponseEntity.ok("started");
     }
 }
